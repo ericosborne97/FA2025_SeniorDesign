5ea525269da751c5024d00007acc878a5d791716
erico <ericosborne97@gmail.com>
2025-09-30 15:26:21 -0400

    Project 2 and Restructuring

--- README.md
+++
@@ -1,2 +0,0 @@
-# FA2025_SeniorDesign
-Senior design repository for group 13, the luckiest group
--- detect_cameras.py
+++
@@ -1,6 +0,0 @@
-'''
-You can use this to get the id of all cameras on your device
-'''
-from cv2_enumerate_cameras import enumerate_cameras
-for cam in enumerate_cameras():           # cross-platform
-    print(cam.index)  # index is what cv2 needs
